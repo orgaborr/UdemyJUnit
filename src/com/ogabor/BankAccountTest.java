9675c17f578b9ff5be6e2a792b70386de7c41e17
Gabor <orbangabeszb@gmail.com>
2019-10-02 19:58:13 +0200

    first commit

---
+++ src/com/ogabor/BankAccountTest.java
@@ -0,0 +1,43 @@
+package com.ogabor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    private BankAccount account;
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setup() {
+        account = new BankAccount("Gábor", "Orbán", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        double balance = account.withdraw(200.00, true);
+        assertEquals(800.00, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_deposit() {
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isChecking_true() {
+        assertTrue(account.isChecking());
+    }
+}
