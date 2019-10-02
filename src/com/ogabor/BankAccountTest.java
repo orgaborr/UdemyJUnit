6d47e08d6abdd3c6525fe28fb6f9d9f80798255d
Gabor <orbangabeszb@gmail.com>
2019-10-02 21:33:16 +0200

    count added to tests

--- src/com/ogabor/BankAccountTest.java
+++ src/com/ogabor/BankAccountTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
 
     private BankAccount account;
+    private static int count;
 
     @org.junit.jupiter.api.BeforeAll
     public static void beforeAll() {
-        System.out.println("This executes before any test cases");
+        System.out.println("This executes before any test cases. Count = " + count++);
     }
 
     @org.junit.jupiter.api.BeforeEach
@@ -48,6 +49,11 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.AfterAll
     public static void afterAll() {
-        System.out.println("This executes after any test cases");
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void teardown() {
+        System.out.println("Count = " + count++);
     }
 }
